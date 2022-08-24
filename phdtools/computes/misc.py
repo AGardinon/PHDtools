ada3962e8977e6fb514778581156ff1c6316ed9a
AGardinon <andreagardin92@gmail.com>
2022-08-24 17:49:59 +0100

    added fist raw version of descriptors to phdtools

--- phdtools/computes/misc.py
+++ phdtools/computes/misc.py
@@ -5,9 +5,13 @@
 # AUTHOR: Andrea Gardin
 # -------------------------------------------------- #
 
-from datetime import date
+# --- custom exception
+class WrongConfigFormat(Exception):
+    pass
 
 # --- get todays date string
+from datetime import date
+
 def todayDate(date_format="%d%b%Y"):
     """
     Gets you todays date in according to the format choosen
@@ -15,3 +19,24 @@ def todayDate(date_format="%d%b%Y"):
     """
     today = date.today()
     return today.strftime(date_format)
+
+# --- parse dict file .json .toml
+import json
+import toml
+from types import SimpleNamespace
+
+def parse_config(filename, toNameSpace=False):
+    with open(filename, 'r') as f:
+        if filename.endswith('.json'):
+            _config = json.load(f)
+        elif filename.endswith('.toml'):
+            _config = toml.load(f)
+        else:
+            raise WrongConfigFormat(
+                f"Format of the '{filename}' is not supported. "
+                "Available formats: .json, .toml"
+            )
+        if toNameSpace:
+            return SimpleNamespace(**_config)
+        else:
+            return _config
