e15dbe1d495b26df5cf79739b2383383e24df35a
AGardinon <andreagardin92@gmail.com>
2023-09-08 15:38:06 +0200

    minor change to setup file (again)

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='phdtools',
-    version='1.0.1',
-    url='https://github.com/AGardinon/PHDtools.git',
+    version='1.0.0',
+    url='https://github.com/AGardinon/PHDtools',
     author='Andrea Gardin',
     description='Generic computational tools for PhD aid :)',
     packages=find_packages(),
